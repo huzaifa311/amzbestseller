3edf114d780bdac8a0fef4a88f78051b6b6e40d7
musmanirfan <usmanmala356@gmail.com>
2025-05-09 17:47:59 -0700

    email chane

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.post("/form", async (req, res) => {
 
         await transporter.sendMail({
             from: "huzaifatemp312@gmail.com",
-            to: "info@amzbestsellers.us",
+            to: "info@amzebookpublication.com",
             subject: "New Form Submission",
             html: `
         <h1>New Form Submission</h1>
